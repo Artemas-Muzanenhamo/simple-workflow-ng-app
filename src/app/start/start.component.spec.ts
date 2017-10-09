aac3c490b1cb409276aa9cb76ca30f82b8b61c4d
Artemas-Muzanenhamo <artemasseng@gmail.com>
2017-10-09 16:28:02 +0100

    Added Start-Process to the Start-Workflow Component

---
+++ src/app/start/start.component.spec.ts
@@ -0,0 +1,17 @@
+import {async, TestBed} from '@angular/core/testing';
+import {StartComponent} from './start.component';
+
+describe('StartComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        StartComponent
+      ],
+    }).compileComponents();
+  }));
+  it('should show a button to start a workflow', async(() => {
+    const fixture = TestBed.createComponent(StartComponent);
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('button#start-process').textContent).toContain('Start Process');
+  }));
+});
---
+++ src/app/start/start.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-start-process',
+  templateUrl: './start.component.html',
+  styleUrls: ['./start.component.css']
+})
+export class StartComponent {}
