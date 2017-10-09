c78aee1cde8acbaeecf21ee0dd7ee1261d1de5fa
Artemas-Muzanenhamo <artemasseng@gmail.com>
2017-10-09 13:50:13 +0100

    First inital Commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,47 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+  it(`should have as title 'Workflow App'`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('Workflow App');
+  }));
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Simple Workflow App!');
+  }));
+  it('should show a button to deploy a process', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('button#deploy-process').textContent).toContain('Deploy Process');
+  }));
+  it('should show a button to start a workflow', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('button#start-process').textContent).toContain('Start Process');
+  }));
+  it('should show a button to find a process', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('button#find-process').textContent).toContain('Find Process');
+  }));
+  it('should show a button to complete a process', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('button#complete-process').textContent).toContain('Complete Process');
+  }));
+});
