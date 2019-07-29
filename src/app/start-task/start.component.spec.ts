ff9a55bee7270e93123fbd9e283e39c4d169e1ef
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-07-29 23:47:30 +0100

    Add missing module to make test pass

--- src/app/start-task/start.component.spec.ts
+++ src/app/start-task/start.component.spec.ts
@@ -2,6 +2,7 @@ import {async, TestBed} from '@angular/core/testing';
 import {StartComponent} from './start.component';
 import {StartService} from './start.service';
 import {HttpClientModule} from '@angular/common/http';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('StartProcessComponent', () => {
   beforeEach(async(() => {
@@ -9,7 +10,9 @@ describe('StartProcessComponent', () => {
       declarations: [
         StartComponent
       ],
-      imports: [HttpClientModule],
+      imports: [
+        HttpClientModule,
+        RouterTestingModule],
       providers: [StartService]
     }).compileComponents();
   }));
